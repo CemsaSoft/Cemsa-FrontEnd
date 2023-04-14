0eba1ee773802a36b1b6f44edc576c4c8f122251
ClaudioOliveda <coliveda@gmail.com>
2023-04-14 18:25:19 -0500

    Se crea componente Registrar Servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrarServicioComponent } from './registrar-servicio/registrar-servicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrarServicioComponent
   ],
   imports: [
     BrowserModule,
